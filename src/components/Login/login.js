b2978880e1c44207c91a95c0d1aa74a2fb7dfeac
Rishabh <rishabh.ahuja@infoincon.in>
2023-07-14 18:21:50 +0530

    update

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,141 +1,261 @@
 
 
 
+// import React, { useState } from 'react';
+// // import { FaGoogle } from "react-icons/fa";
+// import { FaLinkedin } from "react-icons/fa";
+// import { FaFacebook } from "react-icons/fa";
+// import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
+// import './login.css'; 
+// import { usersData } from '../../Models/user';
+// import { useNavigate } from 'react-router-dom';
+// // import back from '../assets/back.jpg';
+
+// const LoginForm = () => {
+
+//   // <img src={back} height={100} width={50}></img>
+
+//   const [username, setUsername] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [error, setError] = useState('');
+//   const users = usersData;
+
+//   const navigate = useNavigate();
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+
+//     // Perform validation
+//     if (username.trim() != '' && password.trim() != '') {
+//       let user = users.filter(x=>x.email==username);
+//       console.log(user);
+//       if(user.length>0 )
+//       {
+//         user = user[0];
+//         console.log(user);
+//         if(user.password==password){
+//           localStorage.setItem("user", JSON.stringify(user));
+//           navigate('/home');
+//           toast.success('Login Success', {
+//             position: toast.POSITION.BOTTOM_CENTER
+//           });
+//         }
+//         else{
+//           toast.error('Wrong credential', {
+//             position: toast.POSITION.BOTTOM_CENTER
+//           });
+//         }
+//       }
+//       else{
+//         toast.error('User doesn\'t exist', {
+//           position: toast.POSITION.BOTTOM_CENTER
+//         });
+//       }
+      
+//     } else {
+//       toast.error('* All fields are required', {
+//         position: toast.POSITION.BOTTOM_CENTER
+//       });
+//     }
+//   };
+
+
+  
+
+//   return (
+//     <div>
+//       <h1 className="text-center">Login</h1>
+//       <hr/>
+//     <form className='login-form' onSubmit={handleSubmit}>
+//         <div className="row">
+//         <div className="col-md-12">
+//           <label htmlFor="username">Username:</label>
+//           <input
+//             type="text"
+//             id="username"
+//             className='form-control'
+//             placeholder="username"
+//             value={username}
+//             onChange={(e) => setUsername(e.target.value)}
+//           />
+       
+//       </div>
+//      <div className= "col-md-12">
+//           <label htmlFor="password" >Password:</label>
+//           <input
+//             type="password"
+//             id="password"
+//             placeholder="*********"
+//             className='form-control'
+//             value={password}
+//             onChange={(e) => setPassword(e.target.value)}
+//           />
+//          </div>
+//         </div>
+//         <button type="submit">Login</button>
+        
+//         <br/>
+//         <div className="noaccount">
+//           <h6>  "Don't have an account?" 
+//           <a className="anotheroption" href="register">  Sign-up</a>
+//           </h6>
+          
+//         </div>
+       
+// <div className="alternateoption">
+
+//   <span>Or</span>
+// </div>
+
+       
+//         <div className="social-icons">
+
+
+//         <button type="button" class="login-with-google-btn" >
+//   Continue with Google</button>
+
+
+        
+//             {/* <FaGoogle className="social-icon" />   */}
+
+
+          
+//           <FaLinkedin className="social-icon" />
+//           <FaFacebook className="social-icon" />
+//         </div>
+//       </form>
+
+//       <div>
+//           <ToastContainer />
+//         </div>
+//     </div>
+//   );
+// };
+
+// export default LoginForm;
+
+
+
+
+
 import React, { useState } from 'react';
-// import { FaGoogle } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa";
+import { FaLinkedin } from 'react-icons/fa';
+import { FaFacebook } from 'react-icons/fa';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import './login.css'; 
+import './login.css';
 import { usersData } from '../../Models/user';
 import { useNavigate } from 'react-router-dom';
-// import back from '../assets/back.jpg';
 
 const LoginForm = () => {
-
-  // <img src={back} height={100} width={50}></img>
-
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const users = usersData;
-
   const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     // Perform validation
-    if (username.trim() != '' && password.trim() != '') {
-      let user = users.filter(x=>x.email==username);
-      console.log(user);
-      if(user.length>0 )
-      {
+    if (username.trim() !== '' && password.trim() !== '') {
+      let user = users.filter((x) => x.email === username);
+
+      if (user.length > 0) {
         user = user[0];
-        console.log(user);
-        if(user.password==password){
-          localStorage.setItem("user", JSON.stringify(user));
-          navigate('/home');
+
+        if (user.password === password) {
+          localStorage.setItem('user', JSON.stringify(user));
+          setIsLoggedIn(true);
           toast.success('Login Success', {
-            position: toast.POSITION.BOTTOM_CENTER
+            position: toast.POSITION.BOTTOM_CENTER,
           });
-        }
-        else{
+        } else {
           toast.error('Wrong credential', {
-            position: toast.POSITION.BOTTOM_CENTER
+            position: toast.POSITION.BOTTOM_CENTER,
           });
         }
-      }
-      else{
+      } else {
         toast.error('User doesn\'t exist', {
-          position: toast.POSITION.BOTTOM_CENTER
+          position: toast.POSITION.BOTTOM_CENTER,
         });
       }
-      
     } else {
       toast.error('* All fields are required', {
-        position: toast.POSITION.BOTTOM_CENTER
+        position: toast.POSITION.BOTTOM_CENTER,
       });
     }
   };
 
-
-  
+  const handleLogout = () => {
+    localStorage.removeItem('user');
+    setIsLoggedIn(false);
+    toast.success('Logout Success', {
+      position: toast.POSITION.BOTTOM_CENTER,
+    });
+  };
 
   return (
     <div>
-      <h1 className="text-center">Login</h1>
-      <hr/>
-    <form className='login-form' onSubmit={handleSubmit}>
-        <div className="row">
-        <div className="col-md-12">
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            className='form-control'
-            placeholder="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-       
-      </div>
-     <div className= "col-md-12">
-          <label htmlFor="password" >Password:</label>
-          <input
-            type="password"
-            id="password"
-            placeholder="*********"
-            className='form-control'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-         </div>
+      {isLoggedIn ? (
+        <div>
+          <h1 className="text-center">Welcome, {localStorage.getItem('user').name}</h1>
+          <button onClick={handleLogout}>Logout</button>
         </div>
-        <button type="submit">Login</button>
-        
-        <br/>
-        <div className="noaccount">
-          <h6>  "Don't have an account?" 
-          <a className="anotheroption" href="register">  Sign-up</a>
-          </h6>
-          
-        </div>
-       
-<div className="alternateoption">
-
-  <span>Or</span>
-</div>
-
-       
-        <div className="social-icons">
-
-
-        <button type="button" class="login-with-google-btn" >
-  Continue with Google</button>
-
-
-        
-            {/* <FaGoogle className="social-icon" />   */}
-
-
-          
-          <FaLinkedin className="social-icon" />
-          <FaFacebook className="social-icon" />
-        </div>
-      </form>
-
-      <div>
+      ) : (
+        <div>
+          <h1 className="text-center">Login</h1>
+          <hr />
+          <form className="login-form" onSubmit={handleSubmit}>
+            <div className="row">
+              <div className="col-md-12">
+                <label htmlFor="username">Username:</label>
+                <input
+                  type="text"
+                  id="username"
+                  className="form-control"
+                  placeholder="username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+              <div className="col-md-12">
+                <label htmlFor="password">Password:</label>
+                <input
+                  type="password"
+                  id="password"
+                  placeholder="*********"
+                  className="form-control"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </div>
+            <button type="submit">Login</button>
+            <br />
+            <div className="noaccount">
+              <h6>
+                "Don't have an account?"
+                <a className="anotheroption" href="register">
+                  Sign-up
+                </a>
+              </h6>
+            </div>
+            <div className="alternateoption">
+              <span>Or</span>
+            </div>
+            <div className="social-icons">
+              <FaLinkedin className="social-icon" />
+              <FaFacebook className="social-icon" />
+            </div>
+          </form>
           <ToastContainer />
         </div>
+      )}
     </div>
   );
 };
 
 export default LoginForm;
-
-
-
-
-
--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,55 +1,115 @@
 
-import React, { useState, useEffect } from 'react';
+// import { useParams } from 'react-router-dom';
+// import productsdata from './ProductList';
+// import { FaPlus, FaMinus } from "react-icons/fa";
+
+// const ProductDetails = () => {
+//   let productid = useParams();
+// let obj={};
+// let count=0;
+// const increase=()=>{
+//   count= count + 1
+// }
+
+// const decrease=()=>{
+//   count= count - 1
+// }
+
+// let productList = productsdata.filter(x=>x.id==productid.id);
+// console.log(productList);
+// if(!!productList){
+//   obj = productList[0];
+// console.log(obj);
+// }
+
+//   return (
+//     <div className='container'>
+//       <div className='row'>
+//         <div className='col-md-4'>
+//           <div className='card'>
+//             <img src={obj.imgsrc} alt='product' className='img-responsive'/>
+//           </div>
+//         </div>
+//         <div className='col-md-8'>
+//           <h2>{obj.name}</h2>
+//           <p>{obj.description}</p>
+//           <p>${obj.price}</p>
+//           <p>Stars: {obj.ratings}</p>
+//           <div className='box'>
+//             <button className='btn btn-outline'  onClick={increase}>
+//               <FaPlus/>
+//             </button>
+//             {count}
+          
+//             <button className='btn btn-outline'  onClick={decrease}>
+//             <FaMinus/>
+
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+      
+//     </div>
+//   );
+// };
+
+// export default ProductDetails;
+
+
+
+
+
+
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import productsdata from './ProductList';
-import { FaPlus, FaMinus } from "react-icons/fa";
+import { FaPlus, FaMinus } from 'react-icons/fa';
 
 const ProductDetails = () => {
-  let productid = useParams();
-let obj={};
-let count=0;
-const increase=()=>{
-  count= count + 1
-}
+  const { id } = useParams();
+  const [count, setCount] = useState(0);
 
-const decrease=()=>{
-  count= count - 1
-}
+  const increase = () => {
+    setCount(count + 1);
+  };
 
-let productList = productsdata.filter(x=>x.id==productid.id);
-console.log(productList);
-if(!!productList){
-  obj = productList[0];
-console.log(obj);
-}
+  const decrease = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  const productList = productsdata.filter((x) => x.id === parseInt(id));
+  const obj = productList.length > 0 ? productList[0] : null;
 
   return (
     <div className='container'>
-      <div className='row'>
-        <div className='col-md-4'>
-          <div className='card'>
-            <img src={obj.imgsrc} alt='product' className='img-responsive'/>
+      {obj ? (
+        <div className='row'>
+          <div className='col-md-4'>
+            <div className='card'>
+              <img src={obj.imgsrc} alt='product' className='img-responsive' />
+            </div>
+          </div>
+          <div className='col-md-8'>
+            <h2>{obj.name}</h2>
+            <p>{obj.description}</p>
+            <p>${obj.price}</p>
+            <p>Stars: {obj.ratings}</p>
+            <div className='box'>
+              <button className='btn btn-outline' onClick={increase}>
+                <FaPlus />
+              </button>
+              {count}
+              <button className='btn btn-outline' onClick={decrease}>
+                <FaMinus />
+              </button>
+            </div>
           </div>
         </div>
-        <div className='col-md-8'>
-          <h2>{obj.name}</h2>
-          <p>{obj.description}</p>
-          <p>${obj.price}</p>
-          <p>Stars: {obj.ratings}</p>
-          <div className='box'>
-            <button className='btn btn-outline'  onClick={increase}>
-              <FaPlus/>
-            </button>
-            {count}
-          
-            <button className='btn btn-outline'  onClick={decrease}>
-            <FaMinus/>
-
-            </button>
-          </div>
-        </div>
-      </div>
-      
+      ) : (
+        <p>Product not found.</p>
+      )}
     </div>
   );
 };
